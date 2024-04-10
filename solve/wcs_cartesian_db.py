6a588e6ad7ffdc6e7c903aa603825629df050a96
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2024-04-10 12:27:53 +0800

    计算ok

--- solve/wcs_cartesian_db.py
+++ solve/wcs_cartesian_db.py
@@ -8,6 +8,18 @@ import numpy as np
 from numpy.linalg import inv
 
 
+def vector_angle_deg(u_x, u_y, u_z, v_x, v_y, v_z):
+    # 计算点积
+    dot_product = u_x * v_x + u_y * v_y + u_z * v_z
+    # 计算余弦值
+    cos_theta = dot_product
+    # 计算夹角的弧度值
+    theta = math.acos(cos_theta)
+    theta_degrees = math.degrees(theta)
+
+    return theta_degrees
+
+
 # 打开 FITS 文件
 # hdul = fits.open(r"E:/testimg/tycho/GY1_K008-5_No Filter_60S_Bin2_UTC20231218_141114_-25C_.fit")
 hdul = fits.open(r"E:/testimg/tycho/GY1_K040-6_No Filter_60S_Bin2_UTC20231010_200646_-19.9C_.fit")
@@ -29,12 +41,12 @@ print('-----------------')
 image_data = hdul[0].data
 # 获取图像的宽度和高度
 width, height = image_data.shape[1], image_data.shape[0]
-print(f'x: {width}  y:{height}    x/2 {(width+1)/2}   y/2 {(height+1)/2}')
+print(f'x: {width}  y:{height}    x/2 {(width + 1) / 2}   y/2 {(height + 1) / 2}')
 # 获取x y中点
-ra_mid_x, dec_mid_x = wcs_info.wcs_pix2world((width+1)/2, 0, 1)
-ra_mid_y, dec_mid_y = wcs_info.wcs_pix2world(0, (height+1)/2, 1)
+ra_mid_x, dec_mid_x = wcs_info.wcs_pix2world((width + 1) / 2, 0, 1)
+ra_mid_y, dec_mid_y = wcs_info.wcs_pix2world(0, (height + 1) / 2, 1)
 #  tycho 的识别结果有时候不是以图像中心点为 crpix ,会有少量偏移?
-ra_mid_img, dec_mid_img = wcs_info.wcs_pix2world((width+1)/2, (height+1)/2, 1)
+ra_mid_img, dec_mid_img = wcs_info.wcs_pix2world((width + 1) / 2, (height + 1) / 2, 1)
 ra_corner_img, dec_corner_img = wcs_info.wcs_pix2world(0, 0, 1)
 print(f'x_mid: {ra_mid_x}  {dec_mid_x}    y_mid: {ra_mid_y}  {dec_mid_y}   img_mid: {ra_mid_img}  {dec_mid_img}  ')
 
@@ -47,33 +59,27 @@ coord_img_center = SkyCoord(ra=ra_mid_img, dec=dec_mid_img, unit='deg')
 cartesian_img_center = coord_img_center.cartesian
 coord_img_corner = SkyCoord(ra=ra_corner_img, dec=dec_corner_img, unit='deg')
 cartesian_img_corner = coord_img_corner.cartesian
+coord_mid_x = SkyCoord(ra=ra_mid_x, dec=dec_mid_x, unit='deg')
+cartesian_mid_x = coord_mid_x.cartesian
+coord_mid_y = SkyCoord(ra=ra_mid_y, dec=dec_mid_y, unit='deg')
+cartesian_mid_y = coord_mid_y.cartesian
 
-print(f'center {cartesian_img_center}   ')
+print(f'center {coord_img_center}   {cartesian_img_center}   ')
 
 # 计算两个点之间的角距离（以弧度为单位）
 angle_deg = coord_img_center.separation(coord_img_corner)
 
 # 输出结果
-print(f"两个点之间的夹角（度）: {angle_deg} 度")
+print(f"astropy separation: {angle_deg} 度")
 
-u_x, u_y, u_z = coord_img_center.cartesian.x, coord_img_center.cartesian.y, coord_img_center.cartesian.z
-v_x, v_y, v_z = coord_img_corner.cartesian.x, coord_img_corner.cartesian.y, coord_img_corner.cartesian.z
+theta_deg = vector_angle_deg(cartesian_img_corner.x, cartesian_img_corner.y, cartesian_img_corner.z,
+                             cartesian_img_center.x, cartesian_img_center.y, cartesian_img_center.z)
+print(f"img corner deg: {theta_deg}   {coord_img_corner}  {cartesian_img_corner}  to {cartesian_img_center} ")
 
-# 计算点积
-dot_product = u_x * v_x + u_y * v_y + u_z * v_z
+theta_deg = vector_angle_deg(cartesian_mid_x.x, cartesian_mid_x.y, cartesian_mid_x.z,
+                             cartesian_img_center.x, cartesian_img_center.y, cartesian_img_center.z)
+print(f"img width deg: {theta_deg}   {coord_mid_x}  {cartesian_mid_x}  to {cartesian_img_center} ")
 
-# 计算余弦值
-cos_theta = dot_product
-
-# 计算夹角的弧度值
-theta = math.acos(cos_theta)
-
-# 如果需要，将弧度转换为度数
-theta_degrees = math.degrees(theta)
-
-# 输出结果
-print(f"两个单位向量之间的夹角（弧度）: {theta}")
-print(f"两个单位向量之间的夹角（度）: {theta_degrees} 度")
-
-
-
+theta_deg = vector_angle_deg(cartesian_mid_y.x, cartesian_mid_y.y, cartesian_mid_y.z,
+                             cartesian_img_center.x, cartesian_img_center.y, cartesian_img_center.z)
+print(f"img height deg: {theta_deg}   {coord_mid_y}  {cartesian_mid_y}  to {cartesian_img_center} ")
