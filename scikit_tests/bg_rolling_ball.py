dd4fd8c89051465db27a7b10e20fb70ef13b1f15
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2024-06-26 17:33:52 +0800

    scikit test

---
+++ scikit_tests/bg_rolling_ball.py
@@ -0,0 +1,63 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pywt
+from skimage import restoration
+from astropy.io import fits  # 导入用于读取FITS文件的模块
+from skimage.util import img_as_float  # 导入用于将图像转换为浮点格式的函数
+matplotlib.use('TkAgg')
+
+
+def plot_result(image, background):
+    fig, ax = plt.subplots(nrows=1, ncols=3)
+
+    ax[0].imshow(image, cmap='gray')
+    ax[0].set_title('Original image')
+    ax[0].axis('off')
+
+    ax[1].imshow(background, cmap='gray')
+    ax[1].set_title('Background')
+    ax[1].axis('off')
+
+    ax[2].imshow(image - background, cmap='gray')
+    ax[2].set_title('Result')
+    ax[2].axis('off')
+
+    fig.tight_layout()
+
+
+# 读取FITS文件
+fits_file = f'sub_fits.fits'
+with fits.open(fits_file) as hdul:
+    image_FITS = hdul[0].data  # 假设数据存储在HDU 0中
+
+# 将16位数据转换为浮点格式
+image_fits = img_as_float(image_FITS)
+
+# 应用滚动球算法估计背景
+background = restoration.rolling_ball(image_fits)
+# 计算结果图像
+result_image = image_fits - background
+
+# 创建包含背景和结果图像的HDU列表
+hdu_background_p = fits.PrimaryHDU(background)
+hdu_background = fits.HDUList(hdu_background_p)
+
+hdu_result_p = fits.PrimaryHDU(result_image)
+hdu_result = fits.HDUList(hdu_result_p)
+
+background_fits_file = 'background.fits'
+result_fits_file = 'result.fits'
+
+hdu_background.writeto(background_fits_file, overwrite=True)
+print(f'Background image saved as {background_fits_file}')
+
+hdu_result.writeto(result_fits_file, overwrite=True)
+print(f'Result image saved as {result_fits_file}')
+# 绘制结果
+plot_result(image_fits, background)
+plt.show()
+
+
+print(f'--')
+
---
+++ scikit_tests/sub_fits.py
@@ -0,0 +1,17 @@
+from astropy.io import fits
+import numpy as np
+
+# 指定FITS文件路径
+fits_file_path = f'E:/fix_data/light_1.fits'
+output_fits_file_path = 'sub_fits.fits'
+
+# 打开FITS文件
+with fits.open(fits_file_path) as hdul:
+    # 读取数据，假设数据存储在第一个HDU中
+    image_data = hdul[0].data
+    cropped_data = image_data[:400, :400]
+
+    new_hdu = fits.PrimaryHDU(cropped_data)
+    hdul_new = fits.HDUList(new_hdu)
+    hdul_new.writeto(output_fits_file_path, overwrite=True)
+    print(f"截取的图像已保存到 {output_fits_file_path}")
