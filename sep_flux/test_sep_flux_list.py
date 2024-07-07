20aa182bf8f046db07f1114e0242d832879e9b36
278261631 <mars.maya.g@gmail.com>
2024-07-07 23:52:59 +0800

    似乎第二遍截图有问题

--- sep_flux/test_sep_flux_list.py
+++ sep_flux/test_sep_flux_list.py
@@ -62,7 +62,6 @@ hdulist_mbg = fits.HDUList([hdu_m_bg])
 hdulist_mbg.writeto('sub_fits_mbg.fits', overwrite=True)
 
 
-
 src_string_hms_dms = '16:22:54.448 -16:11:0.93'
 src_string_ra_dec = ''
 ra, dec = get_ra_dec_from_string(src_string_hms_dms, src_string_ra_dec)
@@ -164,9 +163,9 @@ for file_index, file in enumerate(files):
                 # item_ra, item_dec = wcs_info.pixel_to_world([[obj['x'], obj['y']]])[0]
                 # print(f'ra_dec pixel_to_world   {item_ra}   {item_dec}')
                 if s_key not in source_map:
-                    source_map[s_key] = [obj['flux']]
+                    source_map[s_key] = [[fits_id, obj['x'], obj['y'], s_center_x, s_center_y, obj['flux'], obj['a']]]
                 else:
-                    source_map[s_key].append(obj['flux'])
+                    source_map[s_key].append([fits_id, obj['x'], obj['y'], s_center_x, s_center_y, obj['flux'], obj['a']])
 
 
         ax.axis('off')
@@ -183,4 +182,80 @@ with open(flux_txt_full_path, 'w', encoding='utf-8') as file:
     for key, value in sorted_dict.items():
         # 将键值对写入文件，键和值之间用等号连接，然后换行
         file.write(f"{key} = {value}\n")
+
+for file_index, file in enumerate(files):
+    if file.endswith('.txt'):
+        fits_id = file.replace('.txt', '')
+        fits_file_name = f'{fits_id}.fits'
+        png_file_name = f'all_{fits_id}.png'
+        fits_full_path = os.path.join(file_root, fits_file_name)
+        png_all_full_path = os.path.join(file_root, png_file_name)
+        txt_full_path = os.path.join(file_root, file)
+        print(f'++ {fits_file_name}')
+        with open(txt_full_path, 'r', encoding='utf-8') as txt_file:
+            line = txt_file.readline()
+            wcs_info = wcs.WCS(line)
+        pix_xy = wcs_info.world_to_pixel(item_coord)
+        print(f'pix:   {pix_xy}')
+
+        # 打开FITS文件
+        hdu = fits.open(fits_full_path)
+
+        # 获取图像数据，假设它存储在主HDU中
+        data = hdu[0].data
+        hdu.close()
+        # 将16位数据转换为浮点格式
+        data = img_as_float(data)
+
+        height, width = data.shape
+        center_x = pix_xy[0]
+        center_y = pix_xy[1]
+        start_x = max(center_x - img_sub_x_wid // 2, 0)
+        end_x = min(center_x + img_sub_x_wid // 2 + img_sub_x_wid % 2, width)
+        start_y = max(center_y - img_sub_y_wid // 2, 0)
+        end_y = min(center_y + img_sub_y_wid // 2 + img_sub_y_wid % 2, height)
+        start_x = math.floor(start_x)
+        start_y = math.floor(start_y)
+        end_x = math.floor(end_x)
+        end_y = math.floor(end_y)
+
+        x_offset = abs(min(center_x - img_sub_x_wid // 2, 0))
+        y_offset = abs(min(center_y - img_sub_y_wid // 2, 0))
+        x_offset = math.floor(x_offset)
+        y_offset = math.floor(y_offset)
+
+        mark_position_x = center_x - start_x + x_offset
+        mark_position_y = center_y - start_y + y_offset
+
+        print(f'{start_x}  {end_x} [{end_x - start_x}]            {start_y} {end_y} [{end_y - start_y}]')
+        region = data_no_bg[start_y:end_y, start_x:end_x]
+        # 创建一个新的图像数组，大小为期望的尺寸，初始填充为0（或其他背景值）
+        new_image = np.zeros((img_sub_y_wid, img_sub_x_wid), dtype=data.dtype)
+
+        new_image[y_offset:y_offset + region.shape[0], x_offset:x_offset + region.shape[1]] = region
+
+        fig, ax = plt.subplots()
+        ax.imshow(new_image, cmap='gray')
+
+        # circle = Circle((mark_position_x, mark_position_y), 10, edgecolor='green', facecolor='none', linewidth=0.4)
+        # ax.add_patch(circle)
+
+        # 在图像上为每个源绘制圆圈
+        for item_key, item_value in sorted_dict.items():
+            for val_index, sources_val in enumerate(item_value):
+
+                s_center_x = sources_val[3]
+                s_center_y = sources_val[4]
+                if fits_id == sources_val[0]:
+                    # 圆圈的半径，这里使用a参数的一半作为圆圈半径
+                    radius = sources_val[6] * 3.0
+                    # 绘制圆圈
+                    circle = plt.Circle((s_center_x, s_center_y), radius, color='yellow', fill=False, linewidth=0.2)
+                    ax.add_patch(circle)
+
+        ax.axis('off')
+
+        plt.savefig(png_all_full_path, dpi=300, format='png', bbox_inches='tight')
+        plt.close(fig)
+        # break
 
