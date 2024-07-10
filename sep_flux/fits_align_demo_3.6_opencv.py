67688089542a2debbcb105a935eaefbbdd301e3b
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2024-07-10 17:47:52 +0800

    no good

--- sep_flux/fits_align_demo_3.6_opencv.py
+++ sep_flux/fits_align_demo_3.6_opencv.py
@@ -3,29 +3,21 @@ import numpy as np
 from astropy.io import fits
 
 
-def align_fits_images(fits_file1, fits_file2):
+def align_images(image_file1, image_file2):
     """
-    对齐两个仅含有点源的 FITS 图像，即使只有少部分重叠。
+    对齐两个仅含有点源的图像，即使只有少部分重叠。
 
     Args:
-        fits_file1 (str): 第一个 FITS 文件路径。
-        fits_file2 (str): 第二个 FITS 文件路径。
+        image_file1 (str): 第一个图像文件路径。
+        image_file2 (str): 第二个图像文件路径。
 
     Returns:
         tuple: 包含两个对齐后的图像的元组 (aligned_image1, aligned_image2)。
     """
 
-    # 加载 FITS 图像
-    hdu1 = fits.open(fits_file1)
-    image1 = hdu1[0].data
-    image1 = image1.astype(np.float32)
-    hdu2 = fits.open(fits_file2)
-    image2 = hdu2[0].data
-    image2 = image2.astype(np.float32)
-
-    # 将 FITS 数据转换为 OpenCV 格式
-    image1 = image1.astype(np.uint8)
-    image2 = image2.astype(np.uint8)
+    # 加载图像
+    image1 = cv2.imread(image_file1, cv2.IMREAD_GRAYSCALE)
+    image2 = cv2.imread(image_file2, cv2.IMREAD_GRAYSCALE)
 
     # 使用 SIFT 检测点源
     sift = cv2.SIFT_create()
@@ -60,21 +52,15 @@ def align_fits_images(fits_file1, fits_file2):
     image1_with_keypoints = cv2.drawKeypoints(aligned_image1, keypoints1, None, color=(0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     image2_with_keypoints = cv2.drawKeypoints(aligned_image2, keypoints2, None, color=(0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    # return aligned_image1, aligned_image2
     return image1_with_keypoints, image2_with_keypoints
 
+
 # 示例用法
-fits_file1 = "src_process/test_/109120220407205235.fits"
-fits_file2 = "src_process/test_/209120220407205125.fits"
-# hdu1 = fits.open('src_process/test_/109120220506194824.fits')
-# # hdu1 = fits.open('src_process/test_/109120220407205235.fits')
-# hdu2 = fits.open('src_process/test_/209120220407205125.fits')
-# aligned_image1, aligned_image2 = align_fits_images(fits_file1, fits_file2)
-image1_with_keypoints, image2_with_keypoints = align_fits_images(fits_file1, fits_file2)
+image_file1 = "src_process/test_/s_109120220407205235.png"
+image_file2 = "src_process/test_/s_209120220407205125.png"
+image1_with_keypoints, image2_with_keypoints = align_images(image_file1, image_file2)
 
 # 显示结果
-# cv2.imshow("Aligned Image 1", aligned_image1)
-# cv2.imshow("Aligned Image 2", aligned_image2)
 cv2.imshow("Aligned Image 1 with Keypoints", image1_with_keypoints)
 cv2.imshow("Aligned Image 2 with Keypoints", image2_with_keypoints)
 cv2.waitKey(0)
