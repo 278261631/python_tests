55ef1daa28ba1f439f79b2cc46e1a3eea2c841ea
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2024-07-12 17:48:39 +0800

    try fits align by sift

---
+++ sep_flux/test_sep_flux_align_fits_by_sift.py
@@ -0,0 +1,100 @@
+import math
+import os
+
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import sep
+from PIL import Image
+from astropy.coordinates import SkyCoord
+from astropy.io import fits
+from astropy.visualization import PercentileInterval, LinearStretch, ImageNormalize
+from matplotlib.patches import Circle
+from skimage.util import img_as_float
+from tools.ra_dec_tool import get_ra_dec_from_string
+from astropy import wcs
+matplotlib.use('TkAgg')
+
+
+src_string_hms_dms = '16:22:54.448 -16:11:0.93'
+src_string_ra_dec = ''
+ra, dec = get_ra_dec_from_string(src_string_hms_dms, src_string_ra_dec)
+
+file_root = f'src_process/{ra:0>3.6f}_{dec:0>2.8f}/'
+item_coord = SkyCoord(ra=ra, dec=dec, unit='deg')
+files = os.listdir(file_root)
+
+for file_index, file in enumerate(files):
+    if file.endswith('.txt'):
+        fits_id = file.replace('.txt', '')
+        fits_file_name = f'{fits_id}.fits'
+        png_file_name = f'{fits_id}.png'
+        fits_full_path = os.path.join(file_root, fits_file_name)
+        png_full_path = os.path.join(file_root, png_file_name)
+        scal_png_full_path = os.path.join(file_root, f'stretch_{fits_id}.png')
+        dot_png_full_path = os.path.join(file_root, f'dot_{fits_id}.png')
+        txt_full_path = os.path.join(file_root, file)
+        print(f'++ {fits_file_name}')
+        with open(txt_full_path, 'r', encoding='utf-8') as txt_file:
+            line = txt_file.readline()
+            wcs_info = wcs.WCS(line)
+        # print(f'{line}')
+        # print(f'{item_coord}')
+        pix_xy = wcs_info.world_to_pixel(item_coord)
+        print(f'pix:   {pix_xy}')
+
+        # 打开FITS文件
+        hdu = fits.open(fits_full_path)
+
+        # 获取图像数据，假设它存储在主HDU中
+        data = hdu[0].data
+        hdu.close()
+        # 将16位数据转换为浮点格式
+        data = img_as_float(data)
+
+        # Stretch to png
+        hist, bin_edges = np.histogram(data, bins=256)
+        peak_index = np.argmax(hist)
+        peak_value = bin_edges[peak_index]
+        interval = PercentileInterval(99.5)  # 这里选择 95% 的范围
+        vmin, vmax = interval.get_limits(data)
+        print(f'vmin: {vmin}   vmax:{vmax}')
+        stretch = LinearStretch()
+        norm = ImageNormalize(data, stretch=stretch, vmin=vmin, vmax=vmax)
+
+        image_data = norm(data) * 255
+        image_data = np.clip(image_data, 0, 255)
+        image_data = image_data.astype(int)
+        print(f'[{np.max(image_data)}]   [{np.min(image_data)}]')
+
+        image_data = image_data.astype(np.uint8)
+        # image = Image.fromarray(data)
+        image_scal = Image.fromarray(image_data)
+
+        # 保存为PNG图像
+        # image.save(png_full_path)
+        image_scal.save(scal_png_full_path)
+
+        # 使用SEP进行源检测
+        bkg = sep.Background(data)
+        bkg_image = bkg.back()
+        data_no_bg = data - bkg_image
+        objects = sep.extract(data_no_bg, 1.5, err=bkg.globalrms)
+        print(f'objs = {len(objects)}')
+        objects1 = []
+        for obj in objects:
+            if obj['flux'] > 5000 or obj['a'] > 100:
+                objects1.append(obj)
+
+        # img1_with_keypoints = image_scal.copy()
+        # for obj1 in objects1:
+        #     cv2.circle(img1_with_keypoints, (int(obj1['x']), int(obj1['y'])), int(obj1['a']), (0, 255, 0), 2)
+
+        img1_with_dot = np.zeros(data.shape, dtype=np.uint8)
+        for obj in objects1:
+            cv2.circle(img1_with_dot, (int(obj['x']), int(obj['y'])), 50, (255, 255, 255), -1)
+        cv2.imwrite(dot_png_full_path, img1_with_dot)
+
+
+
