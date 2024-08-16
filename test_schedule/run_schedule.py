aa73cce22981b55d770237e62aa861d422ef2a72
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2024-08-16 17:33:53 +0800

    test_schedule

---
+++ test_schedule/run_schedule.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+
+
+def job():
+    print("I'm working...")
+
+
+schedule.every(5).seconds.do(job)
+
+schedule.every().day.at("10:00:00").do(job)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
