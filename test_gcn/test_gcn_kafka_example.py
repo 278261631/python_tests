66d6ecd517dfeaa80603005d14170026ee2bd399
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2025-03-10 12:58:04 +0800

    显示和计数

--- test_gcn/test_gcn_kafka_example.py
+++ test_gcn/test_gcn_kafka_example.py
@@ -1,3 +1,6 @@
+import time
+from datetime import timedelta
+
 from gcn_kafka import Consumer
 import json
 
@@ -9,7 +12,7 @@ with open('config.json') as f:
 consumer = Consumer(client_id=config['client_id'],
                     client_secret=config['client_secret'])
 # 以上内容改成从文本获取
-
+start_time = time.time()
 
 # Subscribe to topics and receive alerts
 consumer.subscribe(['gcn.circulars',
@@ -18,12 +21,33 @@ consumer.subscribe(['gcn.circulars',
                     'igwn.gwalert',
                     'gcn.notices.swift.bat.guano',
                     'gcn.notices.einstein_probe.wxt.alert'])
+heart_beat_count = 0
 while True:
     for message in consumer.consume(timeout=1):
+        elapsed_time = time.time() - start_time
         if message.error():
             print(message.error())
             continue
-        # Print the topic and message ID
-        print(f'topic={message.topic()}, offset={message.offset()}')
-        value = message.value()
-        print(value)
+        if message.topic() == 'gcn.heartbeat':
+            heart_beat_count += 1
+            formatted_time = str(timedelta(seconds=elapsed_time)).split(".")[0]  # 格式化为HH:MM:SS
+
+            # print(f'topic={message.topic()}, offset={message.offset()}')
+
+            # 12 hour
+            if heart_beat_count % 43200 == 0:
+                print('\r 12 H pass')
+            print(f'\rHeartbeat {heart_beat_count}    {formatted_time}', end='', flush=True)
+
+
+            continue
+        if message.topic() == 'gcn.notices.einstein_probe.wxt.alert':
+            print(f'topic={message.topic()}, offset={message.offset()}')
+            # Print the topic and message ID
+            value = message.value()
+            print(f'\r-------------')
+            print(value)
+            print(f'-------------')
+        else:
+            print(f'topic={message.topic()}, offset={message.offset()}, elapsed_time={elapsed_time}')
+
