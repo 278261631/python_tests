6ce7a5c12f21aecd915838f5741bcd7876cf12b8
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2025-04-17 17:32:16 +0800

    multi_core multi_task

--- multi_task/multi_task_ui.py
+++ multi_task/multi_task_ui.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import subprocess
 
 import psutil
 import os
@@ -23,7 +24,7 @@ def get_cpu_info():
     return current_core, [c for c in range(total_cores) if c != current_core]
 
 
-def worker_task(core_id, task_name):
+def worker_task(core_id, command_args):
     # 绑定到指定核心
     process = psutil.Process(os.getpid())
     try:
@@ -37,14 +38,16 @@ def worker_task(core_id, task_name):
     current_core = os.sched_getcpu() if hasattr(os, 'sched_getcpu') else \
         multiprocessing.current_process()._identity[0] % psutil.cpu_count()
 
-    # 模拟计算任务
-    result = 0
-    for i in range(10_000_000):
-        result += i % 100
-    print(f"任务 {task_name} 在核心 {current_core} 完成，结果: {result}")
+    # 启动python 或者其它程序
+    result = subprocess.run(
+        command_args,
+        capture_output=True,
+        text=True
+    )
+    print(f"任务 {command_args} 在核心 {current_core} 结束")
 
 
-def create_tasks():
+def create_tasks(command_args):
     # 获取可用核心
     _, available_cores = get_cpu_info()
     if len(available_cores) < 2:
@@ -56,25 +59,23 @@ def create_tasks():
     # 创建进程
     p1 = multiprocessing.Process(
         target=worker_task,
-        args=(core1, "A"),
+        args=(core1, command_args),
         name="Core{}_Task".format(core1)
     )
-    p2 = multiprocessing.Process(
-        target=worker_task,
-        args=(core2, "B"),
-        name="Core{}_Task".format(core2)
-    )
-
     # 启动并等待
     p1.start()
-    p2.start()
     p1.join()
-    p2.join()
+
 
 
 if __name__ == "__main__":
     current, others = get_cpu_info()
     print(f"当前核心: {current}")
     print(f"其他核心: {others}")
-    create_tasks()
+    commands = [
+        f"python test_tasks.py prime 1000000 6000001",
+        f"python test_tasks.py pi 25000",
+        ]
+    create_tasks(commands[0])
+    create_tasks(commands[1])
 
