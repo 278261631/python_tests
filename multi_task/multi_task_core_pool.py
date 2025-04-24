de3e22a8e42dbc5ffa47a61aa0ded2d1569abfac
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2025-04-24 13:20:55 +0800

    保存日志, 准备做lib

--- multi_task/multi_task_core_pool.py
+++ multi_task/multi_task_core_pool.py
@@ -2,6 +2,7 @@ import datetime
 import multiprocessing
 import subprocess
 import sys
+from logging.handlers import RotatingFileHandler
 
 import psutil
 import os
@@ -9,22 +10,51 @@ import os
 import matplotlib
 matplotlib.use('Agg')  # 在导入pyplot之前设置非交互式后端
 import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter
+
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+# logger.setLevel(logging.WARNING)
+
+# 控制台Handler（INFO级别）
+console_handler = logging.StreamHandler()
+# console_handler.setLevel(logging.INFO)
+console_handler.setLevel(logging.WARNING)
+
+os.makedirs('log_core_pool', exist_ok=True)
+os.makedirs('img_core_pool', exist_ok=True)
+# 文件Handler（DEBUG级别）
+file_handler = RotatingFileHandler(
+    'log_core_pool/task.log',
+    maxBytes=10*1024*1024,
+    backupCount=5,
+    encoding='utf-8'
+)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
 
 def plot_timeline(task_history, filename="task_timeline.png"):
-    print(f"任务 {task_history} ")
+    logging.info(f"任务 {task_history} ")
     plt.figure(figsize=(12, len(task_history) * 0.5 + 2))
 
     # 转换时间格式
     min_time = min(t['start'] for t in task_history)
     max_time = max(t['end'] for t in task_history)
-    print(f"时间范围: {min_time} - {max_time}")
+    logging.warning(f"时间范围: {min_time} - {max_time}")
 
     for i, task in enumerate(task_history):
         # 计算相对时间位置
         start = (task['start'] - min_time).total_seconds()
         duration = (task['end'] - task['start']).total_seconds()
-        print(f"任务 {task['main_id']}_{task['sub_id']} 开始时间: {start}  duration [{duration}]")
+        logging.warning(f"任务 {task['main_id']}_{task['sub_id']} 开始时间: {start}  duration [{duration}]")
         duration = max(duration, 2)
         bar_container = plt.barh(
             y=f"Core {task['core']}",
@@ -48,7 +78,7 @@ def plot_timeline(task_history, filename="task_timeline.png"):
     # plt.xscale('log')
     plt.tight_layout()
     plt.savefig(filename)
-    print(f"运行时间线已保存至 {filename}")
+    logging.info(f"运行时间线已保存至 {filename}")
 
 
 def get_cpu_info():
@@ -74,14 +104,13 @@ def get_cpu_info():
 
 
 def worker_loop(core_id, task_queue, task_history):
-
     """核心工作循环"""
     process = psutil.Process(os.getpid())
     try:
         process.cpu_affinity([core_id])
-        print(f"核心 {core_id} 绑定成功")
+        logging.info(f"核心 {core_id} 绑定成功")
     except Exception as e:
-        print(f"核心 {core_id} 绑定失败: {e}")
+        logging.error(f"核心 {core_id} 绑定失败: {e}")
         return
 
     while True:
@@ -102,7 +131,7 @@ def worker_loop(core_id, task_queue, task_history):
                 current_core = psutil.Process().cpu_num()
         else:
             current_core = core_id
-        print(f"time:{start_time} 任务 [{index+1}] {cmd_args} 在核心 {current_core} 开始")
+        logging.info(f"time:{start_time} 任务 [{index+1}] {cmd_args} 在核心 {current_core} 开始")
         for i, cmd_item in enumerate(cmd_args):
             start_time = datetime.datetime.now()
             # 记录任务元数据
@@ -114,7 +143,7 @@ def worker_loop(core_id, task_queue, task_history):
                 "start": start_time,
                 "end": datetime.datetime.now()  # 最后更新真实结束时间
             }
-            print(f"任务 {index+1}_{i+1} 命令: {cmd_item}")
+            logging.info(f"任务 {index+1}_{i+1} 命令: {cmd_item}")
             result = subprocess.run(
                 cmd_item,
                 capture_output=True,
@@ -125,8 +154,8 @@ def worker_loop(core_id, task_queue, task_history):
             end_time = datetime.datetime.now()
             duration = (end_time - start_time).total_seconds()
             task_data["end"] = end_time
-            print(f"time:{end_time} 任务 {cmd_args} 在核心 {current_core} 结束 : {result.stderr}")
-            print(f"任务总耗时: {duration}")
+            logging.info(f"time:{end_time} 任务 {cmd_args} 在核心 {current_core} 结束 : {result.stderr}")
+            logging.info(f"任务总耗时: {duration}")
             task_history.append(task_data)
 
 
@@ -156,7 +185,7 @@ def create_tasks(command_args):
         p.join()
 
     plot_timeline(list(task_history),
-                filename=f"task_timeline_{datetime.datetime.now().strftime('%Y%m%d_%H%M')}.png")
+                filename=f"img_core_pool/task_timeline_{datetime.datetime.now().strftime('%Y%m%d_%H%M')}.png")
 
 if __name__ == "__main__":
     # 初始化核心池
@@ -164,30 +193,29 @@ if __name__ == "__main__":
     max_core_limit = 3
     selected_cores = others[:max_core_limit]
 
-    print(f"当前核心: {current}")
-    print(f"可用核心池: {selected_cores}")
+    logging.warning(f"当前核心: {current}")
+    logging.warning(f"可用核心池: {selected_cores}")
 
-    # 任务配置（使用列表格式更安全）
-    demo_commands = {
-        "commands": [
-            # {"cmd": [f"c:/python/python310/python.exe", "test_tasks.py", "prime", "1000000", "6000001"]},
-            # {"cmd": [f"c:/python/python310/python.exe", "test_tasks.py", "pi", "25000"]},
-            # {"cmd": [f"c:/python/python310/python.exe", "test_tasks.py", "fib", "41"]},
-            # {"cmd": [f"c:/python/python310/python.exe", "test_tasks.py", "matrix", "2000"]},
-            # {"cmd": [f"c:/python/python310/python.exe", "test_tasks.py", "mc", "30000000"]},
-            # {"cmd": [f"c:/python/python310/python.exe test_tasks.py prime 1000000 5000001", f"c:/python/python310/python.exe test_tasks.py pi 10000", f"E:/github/python_tests/multi_task/test_task.exe mc 400000000"]},
-            # {"cmd": [f"c:/python/python310/python.exe test_tasks.py pi 10000",  f"E:/github/python_tests/mu   lti_task/test_task.exe fib 48"]},
-            # {"cmd": [f"c:/python/python310/python.exe test_tasks.py fib 21", f"c:/python/python310/python.exe test_tasks.py matrix 500",  f"E:/github/python_tests/multi_task/test_task.exe prime 1000000 6000001"]},
-            # {"cmd": [f"c:/python/python310/python.exe test_tasks.py matrix 500"]},
-            # {"cmd": [f"c:/python/python310/python.exe test_tasks.py mc 1000000", f"c:/python/python310/python.exe test_tasks.py matrix 500", f"E:/github/python_tests/multi_task/test_task.exe pi 5000000000"]},
-
-            {"cmd": [f"python test_tasks.py prime 1000000 5000001", f"python test_tasks.py pi 10000", f"/home/mars/PycharmProjects/python_tests/multi_task/test_task mc 400000000"]},
-            {"cmd": [f"python test_tasks.py pi 10000",  f"/home/mars/PycharmProjects/python_tests/multi_task/test_task fib 48"]},
-            {"cmd": [f"python test_tasks.py fib 21", f"python test_tasks.py matrix 500",  f"/home/mars/PycharmProjects/python_tests/multi_task/test_task prime 1000000 40000001"]},
-            {"cmd": [f"python test_tasks.py matrix 5000"]},
-            {"cmd": [f"python test_tasks.py mc 5000000", f"python test_tasks.py matrix 500", f"/home/mars/PycharmProjects/python_tests/multi_task/test_task pi 5000000000"]},
-        ]
-    }
+    if sys.platform.startswith(('linux', 'darwin')):  # Linux/MacOS
+        demo_commands = {
+            "commands": [
+                {"cmd": [f"python test_tasks.py prime 1000000 5000001", f"python test_tasks.py pi 10000", f"/home/mars/PycharmProjects/python_tests/multi_task/test_task mc 400000000"]},
+                {"cmd": [f"python test_tasks.py pi 10000",  f"/home/mars/PycharmProjects/python_tests/multi_task/test_task fib 48"]},
+                {"cmd": [f"python test_tasks.py fib 21", f"python test_tasks.py matrix 500",  f"/home/mars/PycharmProjects/python_tests/multi_task/test_task prime 1000000 40000001"]},
+                {"cmd": [f"python test_tasks.py matrix 5000"]},
+                {"cmd": [f"python test_tasks.py mc 5000000", f"python test_tasks.py matrix 500", f"/home/mars/PycharmProjects/python_tests/multi_task/test_task pi 5000000000"]},
+            ]
+        }
+    else:
+        demo_commands = {
+            "commands": [
+                {"cmd": [f"c:/python/python310/python.exe test_tasks.py prime 1000000 5000001", f"c:/python/python310/python.exe test_tasks.py pi 10000", f"E:/github/python_tests/multi_task/test_task.exe mc 400000000"]},
+                {"cmd": [f"c:/python/python310/python.exe test_tasks.py pi 10000",  f"E:/github/python_tests/multi_task/test_task.exe fib 48"]},
+                {"cmd": [f"c:/python/python310/python.exe test_tasks.py fib 21", f"c:/python/python310/python.exe test_tasks.py matrix 500",  f"E:/github/python_tests/multi_task/test_task.exe prime 1000000 6000001"]},
+                {"cmd": [f"c:/python/python310/python.exe test_tasks.py matrix 500"]},
+                {"cmd": [f"c:/python/python310/python.exe test_tasks.py mc 1000000", f"c:/python/python310/python.exe test_tasks.py matrix 500", f"E:/github/python_tests/multi_task/test_task.exe pi 5000000000"]},
+            ]
+        }
 
     # 执行任务
     create_tasks(demo_commands)
